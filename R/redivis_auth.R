7787303a428b8ceb056f39dc69cbc354c2add8c2
Josh Quan <wrathofquan@gmail.com>
2021-03-08 10:45:24 -0800

    documentation

--- R/redivis_auth.R
+++ R/redivis_auth.R
@@ -1,5 +1,12 @@
-#auth_redivis: sets API token
-
-auth_redivis <- function(i) {
+#' redivis_auth
+#'
+#' Authenticate Using Redivis API
+#'
+#' @format API Key as a String:
+#' \describe{
+#'   \item{x}{placeholder}
+#'
+#' }
+redivis_auth <- function(i) {
   Sys.setenv(REDIVIS_API_TOKEN = i)
 }
