b2ae7d2061fcf26f12edce1ce6755e5aeb9d40fa
Josh Quan <wrathofquan@gmail.com>
2021-03-08 10:36:41 -0800

    remove devtools

--- R/auth_redivis.R
+++ R/auth_redivis.R
@@ -1,6 +1,5 @@
 #auth_redivis: sets API token
 
 auth_redivis <- function(i) {
-    devtools::install_github("r-dbi/bigrquery")
-    Sys.setenv(REDIVIS_API_TOKEN = i)
+  Sys.setenv(REDIVIS_API_TOKEN = i)
 }
