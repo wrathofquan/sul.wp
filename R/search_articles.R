6de24d4f1329ad6feeb1573e7010fef49d9205ed
Josh Quan <wrathofquan@gmail.com>
2021-03-10 19:28:21 -0800

    regex

--- R/search_articles.R
+++ R/search_articles.R
@@ -8,11 +8,11 @@
 
 search_articles <- function(query = query, year = NULL){
   if(missing(year)) {
-    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_all` WHERE paragraphs LIKE '%", query,"%'")
+    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_all` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query,"')")
     tb <- redivis.bigrquery::bq_project_query(sql)
     df <- redivis.bigrquery::bq_table_download(tb)
   } else {
-      sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "` WHERE paragraphs LIKE '%", query, "%'")
+      sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query,"')")
       tb <- redivis.bigrquery::bq_project_query(sql)
       df <- redivis.bigrquery::bq_table_download(tb)
   }
