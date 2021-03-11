99d53153bd507aac25772a64b14e308f012e97c8
Josh Quan <wrathofquan@gmail.com>
2021-03-10 16:56:22 -0800

    namespace

--- R/search_articles.R
+++ R/search_articles.R
@@ -1,19 +1,19 @@
 #' search_articles
 #'
-#' Search by keyword across all years or in specific year
+#' Search by Keyword Across all Years or in One Specific Year
 #' @export
 #' @examples
-#' search_articles(query = "China", year = "1977")
+#' search_articles(query = "police", year = "1977")
 
 
 search_articles <- function(query = query, year = NULL){
   if(missing(year)) {
     sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_all` WHERE paragraphs LIKE '%", query,"%'")
-    tb <- bq_project_query(sql)
-    df <- bq_table_download(tb)
+    tb <- redivis.bigrquery::bq_project_query(sql)
+    df <- redivis.bigrquery::bq_table_download(tb)
   } else {
       sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "` WHERE paragraphs LIKE '%", query, "%'")
-      tb <- bq_project_query(sql)
-      df <- bq_table_download(tb)
+      tb <- redivis.bigrquery::bq_project_query(sql)
+      df <- redivis.bigrquery::bq_table_download(tb)
   }
 }
