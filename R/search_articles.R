ff67705011d74267c19b2eb6fa30e9852e4e9b6c
Josh Quan <wrathofquan@gmail.com>
2021-03-11 15:54:53 -0800

    styling

--- R/search_articles.R
+++ R/search_articles.R
@@ -6,18 +6,18 @@
 #' search_articles(query = "police", year = "1977")
 
 
-search_articles <- function(query = query, year = NULL, strip_html = FALSE){
-  if(missing(year)) {
-    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_all` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query,"')")
+search_articles <- function(query = query, year = NULL, strip_html = FALSE) {
+  if (missing(year)) {
+    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_all` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query, "')")
     tb <- redivis.bigrquery::bq_project_query(sql)
     df <- redivis.bigrquery::bq_table_download(tb)
   } else {
-    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query,"')")
+    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "` WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query, "')")
     tb <- redivis.bigrquery::bq_project_query(sql)
     df <- redivis.bigrquery::bq_table_download(tb)
   }
-  if(strip_html == TRUE) {
-    purrr::map_df(df, function(i){
+  if (strip_html == TRUE) {
+    purrr::map_df(df, function(i) {
       df <- gsub("<.*?>", "", i)
     })
   } else {
