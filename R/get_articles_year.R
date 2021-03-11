6d6547334a344d7f0ec1c1c62f98b65dd854ce2e
Josh Quan <wrathofquan@gmail.com>
2021-03-11 15:55:30 -0800

    styling

--- R/get_articles_year.R
+++ R/get_articles_year.R
@@ -6,9 +6,9 @@
 #' get_articles_year("1977")
 
 get_articles_year <- function(year) {
-    sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "`")
-    tb <- redivis.bigrquery::bq_project_query(sql)
-    df <- redivis.bigrquery::bq_table_download(tb)
+  sql <- paste0("SELECT publish_date, section, authors, title, paragraphs FROM `sul.washington_post.articles_", year, "`")
+  tb <- redivis.bigrquery::bq_project_query(sql)
+  df <- redivis.bigrquery::bq_table_download(tb)
 }
 
 
