8e68fb5b9a7f1f8bcf7d675085eaa2556b49475b
Josh Quan <wrathofquan@gmail.com>
2021-09-28 14:12:59 -0700

    remove article_url

--- R/search_articles.R
+++ R/search_articles.R
@@ -17,13 +17,13 @@ search_articles <- function(query = query,
                             strip_html = FALSE,
                             ...) {
   if (missing(year)) {
-    sql <- paste0("SELECT publish_date, section, kicker, authors, title, blurb, paragraphs, article_url
+    sql <- paste0("SELECT id, publish_date, section, kicker, authors, title, blurb, paragraphs, captions
                   FROM `sul.the_washington_post.articles_all`
                   WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query, "') OR REGEXP_CONTAINS (title, '(?i)", query, "') ")
     tb <- redivis.bigrquery::bq_project_query(sql)
     df <- redivis.bigrquery::bq_table_download(tb)
   } else {
-    sql <- paste0("SELECT publish_date, section, kicker, authors, title, blurb, paragraphs, article_url
+    sql <- paste0("SELECT id, publish_date, section, kicker, authors, title, blurb, paragraphs, captions
                   FROM `sul.the_washington_post.articles_", year, "`
                   WHERE REGEXP_CONTAINS (paragraphs, '(?i)", query, "') OR REGEXP_CONTAINS (title, '(?i)", query, "')")
     tb <- redivis.bigrquery::bq_project_query(sql)
